7c331f71d293603369883fcb00ead7ebd69e89dd
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-08-31 09:56:33 -0600

    displaying graduation data as charts
    need refactoring and correct formating

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,6 +5,13 @@ class ResultsController <ApplicationController
   def index
     gon.institution = Institution.find_by(name:params[:college])
     @college = Institution.find_by(name:params[:college])
+    five = FiveYear.five_year_data(params[:college],params[:program])
+    # @last_five = five.collect{|i| [i.year, i.gender]}
+
+    school = SchoolService.new
+    last_five = school.last_five(params[:college], params[:program])
+    @gender_stats = Hash[last_five.group_by{|h| [h[:year], h[:gender]]}.map{|k,v| [k, v.count] }]
   end
 end 
 
+ 
