33277ef94aa28ea755da8855192d172cb0cd5317
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-09-03 11:05:27 -0600

    adds chart for ethnicities

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -12,7 +12,10 @@ class ResultsController <ApplicationController
     last_five = school.last_five(params[:college], params[:program])
     gender = Hash[last_five.group_by{|h| [h[:year], h[:gender]]}.map{|k,v| [k.join(" - "), v.count] }]
 
+    ethnicity = Hash[last_five.group_by{|h| [h[:year], h[:ethnicity]]}.map{|k,v| [k.join(" - "), v.count]}]
+
     @gender_stats= gender.sort_by {|k| k}
+    @ethnicity_stats = ethnicity.sort_by {|k| k}
     #  byebug
   end
 end 
