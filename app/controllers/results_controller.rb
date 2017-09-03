956ba52dbd9f0586db0b40c1fabb9f67e76b2dd1
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-09-03 15:48:30 -0600

    clears old code from results controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,20 +5,6 @@ class ResultsController <ApplicationController
   def index
     gon.institution = Institution.find_by(name:params[:college])
     @college = Institution.find_by(name:params[:college])
-    five = FiveYear.five_year_data(params[:college],params[:program])
-    # @last_five = five.collect{|i| [i.year, i.gender]}
-
-    school = SchoolService.new
-    last_five = school.last_five(params[:college], params[:program])
-    gender = Hash[last_five.group_by{|h| [h[:year], h[:gender]]}.map{|k,v| [k.join(": "), v.count] }]
-
-    ethnicity = Hash[last_five.group_by{|h| [h[:year], h[:ethnicity]]}.map{|k,v| [k.join(": "), v.count]}]
-    age = Hash[last_five.group_by{|h| [h[:year], h[:agedesc]]}.map{|k,v| [k.join(": "), v.count]}]
-
-    @gender_stats= gender.sort_by {|k| k}
-    @ethnicity_stats = ethnicity.sort_by {|k| k}
-    @age_stats = age.sort_by{|k| k}
-    #  byebug
   end
 end 
 
